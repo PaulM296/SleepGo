a89ac47235da9c4fdd1f4d7aaf3071cf710bb131
PaulM <paulmicluta@gmail.com>
2025-03-15 15:59:01 +0200

    Changed the colors of some pages

--- SleepGoFE/client/src/app/features/home/home.component.ts
+++ SleepGoFE/client/src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../layout/footer/footer.component";
 import { HeaderComponent } from "../../layout/header/header.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
